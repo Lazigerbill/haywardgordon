b71277b41f2a5060e3a9fa19864b82c6054d14b6
Lazigerbill <chaukbill@gmail.com>
2016-11-24 00:59:55 -0500

    initial session is set

--- imports/ui/pages/dashboards/totemdash.js
+++ imports/ui/pages/dashboards/totemdash.js
@@ -2,19 +2,31 @@ import './totemdash.html';
 import '../../components/dataCard.js';
 import '../../components/chart.js';
 import '../../components/status_form.js';
+import '../../components/loadingModal.js';
 import { Machines } from '../../../api/machines/machines.js';
 
 // more elegant solution here: subscribe to all data, but only monitor added new data
 // insert newly added data to session, and the entire template/sub template can just feed off session variables
-const startTime = new Date(new Date().setDate(new Date().getDate()-1));
+
 // set startTime so the subsciption will only limit data to within the last day(24hrs)
 // a bit tricky here to do datetime caculation, I've decided to save everything datetime in JS BSON objects 
 // using startTime as a subscription argument, the publication should only return everything within the last 24 hrs
 
 Template.totemdash.onCreated(function(){
-
-	this.subscribe('meterData', startTime, function(){
+	this.subscribe('meterData', function(){
 	    let initializing = true;
+	    	if (initializing) {
+	    		const init = Machines.find({}, {sort: {'ts': -1}, limit: 1}).fetch();
+	    		Session.set({
+	    		    'ts': init[0].ts,
+	    		    'v12': init[0].message.v12,
+	    		    'fq': init[0].message.fq,
+	    		    'apower': init[0].message.apower,
+	    		    'current': init[0].message.current,
+	    		    'state': init[0].state.currentState,
+	    		    'stateChange': init[0].state.stateChange
+	    		});
+	    	}
 	        Machines.find().observeChanges({
 	            added: function(id, doc) {
 	                if (!initializing) {
