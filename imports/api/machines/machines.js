989f0b5b0642d01cd3b6b16b9de52f0fcb0568a2
Lazigerbill <chaukbill@gmail.com>
2016-11-25 02:30:49 -0500

    attempt to fix time filter issue

--- imports/api/machines/machines.js
+++ imports/api/machines/machines.js
@@ -16,8 +16,9 @@ if (Meteor.isServer) {
 Meteor.methods({
 	dataOnDemand: function(date){
 		const startTime = new Date(date);
-		const endTime = new Date(moment(date).endOf('day'));
+		const endTime = moment(date).endOf('day').toDate();
 		const data = Machines.find({'ts': {$gte: startTime, $lte: endTime}}, {sort: {ts: 1}});
+		console.log(startTime, endTime);
 		const chartData = new Array();
 		data.forEach(function(item){
 		    chartData.push([item.ts.getTime(), item.message.apower]);
